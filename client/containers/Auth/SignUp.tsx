841cd217e9aa7157abfbb5aa426f8c139cfcb41f
diego-c <diechaves@gmail.com>
2018-03-21 23:51:06 -0300

    add signup data

--- client/containers/Auth/SignUp.tsx
+++ client/containers/Auth/SignUp.tsx
@@ -6,23 +6,72 @@ export interface signUpProps {
     isSignedIn: boolean
 }
 
+const today: string =
+    new Date().toLocaleDateString('en-US', {
+        year: 'numeric',
+        month: '2-digit',
+        day: '2-digit'
+    }).split('/')
+        .reduce((acc: string[], field: string, index: number) => {
+            if (/\d{4}/.test(field)) {
+                acc.splice(0, 0, field);
+            } else if (index === 0) {
+                acc.splice(1, 0, field);
+            } else {
+                acc.splice(2, 0, field);
+            }
+            return acc;
+        }, [])
+        .join('-')
+
+enum Gender {
+    Male = 'Male',
+    Female = 'Female'
+}
+
 interface signUpState {
+    email: string,
     username: string,
-    password: string
+    password: string,
+    gender?: Gender,
+    birthdate: string
 }
 
 export class SignUp extends React.Component<signUpProps, signUpState> {
 
     state = {
+        email: '',
         username: '',
-        password: ''
+        password: '',
+        gender: Gender.Male,
+        birthdate: today
     }
 
     onInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-        if (e.target.name === 'username') {
-            this.setState({ username: e.target.value });
-        } else {
-            this.setState({ password: e.target.value });
+        switch (e.target.name) {
+            case 'email':
+                this.setState({ email: e.target.value });
+                break;
+
+            case 'username':
+                this.setState({ username: e.target.value });
+                break;
+
+            case 'password':
+                this.setState({ password: e.target.value });
+                break;
+
+            case 'gender':
+                this.setState({ gender: e.target.value === 'Male' ? Gender.Male : Gender.Female });
+                break;
+
+            case 'birthdate':
+                console.log('Birthdate: ' + e.target.value);
+                this.setState({ birthdate: e.target.value });
+                break;
+
+            default:
+                return;
         }
     }
 
@@ -30,10 +79,7 @@ export class SignUp extends React.Component<signUpProps, signUpState> {
         if (this.state.username && this.state.password) {
             console.log('Signing up as: \n' + JSON.stringify(this.state));
 
-            fetch.post('/signup', {
-                username: this.state.username,
-                password: this.state.password
-            })
+            fetch.post('/signup', this.state)
                 .then((res: AxiosResponse) => console.log('From the server: \n' + JSON.stringify(res.data, null, 2)))
                 .catch((err: Error) => console.log('Oops! \n' + err))
         }
@@ -44,6 +90,13 @@ export class SignUp extends React.Component<signUpProps, signUpState> {
             return (
                 <div>
                     <h3>Sign Up</h3>
+                    <p>E-mail:
+                        <input
+                            type="email"
+                            name="email"
+                            onChange={e => this.onInputChange(e)}
+                            value={this.state.email} />
+                    </p>
                     <p>Username:
                         <input
                             type="text"
@@ -57,6 +110,28 @@ export class SignUp extends React.Component<signUpProps, signUpState> {
                             name="password"
                             onChange={e => this.onInputChange(e)}
                             value={this.state.password} />
+                    </p>
+                    <p>Gender:
+                         <input
+                            type="radio"
+                            name="gender"
+                            onChange={e => this.onInputChange(e)}
+                            value={Gender.Male}
+                            checked />
+                        Male
+                        <input
+                            type="radio"
+                            name="gender"
+                            onChange={e => this.onInputChange(e)}
+                            value={Gender.Female} />
+                        Female
+                    </p>
+                    <p>Birthdate:
+                         <input
+                            type="date"
+                            name="birthdate"
+                            onChange={e => this.onInputChange(e)}
+                            value={this.state.birthdate} />
                     </p>
                     <button onClick={this.onSubmit}>
                         Sign Up
